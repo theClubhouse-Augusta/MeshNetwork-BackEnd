be07ce4d0fb829f588d1b463f4784e29110de985
Oscar-21 <austin.conder@outlook.com>
2018-01-29 14:03:56 -0500

    reset password

---
+++ resources/views/emails/resetPassword.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Booking</title>
+</head>
+<style>
+
+</style>
+<body>
+    <p>Here is the temporary password you requested.</p>
+    <strong>Make sure you update your password after logging in</strong>
+    <p>Temporary Password: {{$temp}}</p>
+</body>
+</html>
