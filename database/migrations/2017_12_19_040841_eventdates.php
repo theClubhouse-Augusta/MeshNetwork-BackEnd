20b0fcd326e8c6e2c3143629d1545a3a485717b1
Oscar-21 <austin.conder@outlook.com>
2018-04-24 13:39:22 -0400

    code format

--- database/migrations/2017_12_19_040841_eventdates.php
+++ database/migrations/2017_12_19_040841_eventdates.php
@@ -6,30 +6,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class Eventdates extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('eventdates', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('eventID')->unsigned();
-            $table->dateTime('start');
-            $table->dateTime('end');
-            $table->timestamps();
-            $table->foreign('eventID')->references('id')->on('events')->onDelete('cascade');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('eventdates', function (Blueprint $table) {
+      $table->increments('id');
+      $table->integer('eventID')->unsigned();
+      $table->dateTime('start');
+      $table->dateTime('end');
+      $table->timestamps();
+      $table
+        ->foreign('eventID')
+        ->references('id')
+        ->on('events')
+        ->onDelete('cascade');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('eventdates');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('eventdates');
+  }
 }
